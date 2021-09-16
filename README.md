5dd03ec5e87a6c86330ac095d66d9a96d2c02637
Syed Ali Raza Bokhari <isyedaliraza@gmail.com>
2021-09-16 09:19:44 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# B-Saloons Frontend
---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+	return (
+		<div className="w-full h-full flex justify-center items-center">
+			<h1 className="font-bold text-5xl">B-Saloons Frontend!</h1>
+		</div>
+	)
+}
+
+export default App
